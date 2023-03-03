4d15e069ef5c97a04d7567515d238f1aa24ba4a0
Orjan <orjano18@uia.no>
2023-03-03 11:01:46 +0100

    I AM A CPP GOD

--- code/src/scene_geometry_publisher/src/scene_geometry_publisher.cpp
+++ code/src/scene_geometry_publisher/src/scene_geometry_publisher.cpp
@@ -1,18 +1,47 @@
 #include <memory>
-#invlude <ifstream>
+
 #include <rclcpp/rclcpp.hpp>
 #include <moveit/move_group_interface/move_group_interface.h>
+
 
 
 class ScenePublisher : public rclcpp::Node
 {
   public:
 
-    ///std::str scene_name_ = "noname";
+    std::string scene_name_ = "noname";
+    std::string file_path_ = "~/git/ORJANOS_MASTER_REPO/code/params/scene_geometry.scene";
+
+    ScenePublisher(const rclcpp::NodeOptions & options) : Node("scene_geometry_publisher", options) {
+
+      std::string joint_state_topic_;
+
+      RCLCPP_INFO(get_logger(), "Scene publisher node started.");
+
+      this->get_parameter("joint_state_topic", joint_state_topic_);
+
+      RCLCPP_INFO(this->get_logger(), "joint_state_topic: %s", joint_state_topic_.c_str());
+      
+    }
+
+    void setHomePoseTarget(moveit::planning_interface::MoveGroupInterface & move_group_interface)
+    {
+      // Set a target Pose
+      auto const home_pose = [] {
+        geometry_msgs::msg::Pose msg;
+        msg.position.x = 0.537;
+        msg.position.y = 0.0;
+        msg.position.z = 0.427;
+        msg.orientation.w = -1;
+        return msg;
+      }();
+
+      move_group_interface.setPoseTarget(home_pose);
+    }
 
   
   private:
-    void load_scene(std::string scene_file)
+    /* void load_scene(std::string scene_file)
     {
       // Read scene geometry from file
       std::ifstream file(file_name_);
@@ -27,7 +56,7 @@ class ScenePublisher : public rclcpp::Node
         if (line.empty() || line[0] == '#')
           continue;
         boost::split(tokens, line, boost::is_any_of(" "));
-        if (tokens[0] == "Base_Scene")
+        if (tokens[0] == scene_name_;)
           continue;
         else if (tokens[0] == "*")
         {
@@ -60,24 +89,27 @@ class ScenePublisher : public rclcpp::Node
         }
       }
 
-    }
+    } */
+
 };
 
 int main(int argc, char* argv[])
 {
   // Initialize ROS and create the Node
   rclcpp::init(argc, argv);
-  rclcpp::NodeOptions node_options;
-  node_options.automatically_declare_parameters_from_overrides(true);
-  auto move_group_node = rclcpp::Node::make_shared("hello_moveit", node_options);
+
+  rclcpp::NodeOptions options;
+  options.automatically_declare_parameters_from_overrides(true);
+  options.parameter_overrides({{"joint_state_topic", "vx300/joint_states"}});
+  auto node = std::make_shared<ScenePublisher>(options);
   
   // Create a ROS logger
-  auto const logger = rclcpp::get_logger("hello_moveit");
+  auto const logger = rclcpp::get_logger("scene_geometry_publisher");
 
   // We spin up a SingleThreadedExecutor for the current state monitor to get information
   // about the robot's state.
   rclcpp::executors::SingleThreadedExecutor executor;
-  executor.add_node(move_group_node);
+  executor.add_node(node);
   std::thread([&executor]() { executor.spin(); }).detach();
   
   // MoveIt operates on sets of joints called "planning groups" and stores them in an object called
@@ -86,45 +118,41 @@ int main(int argc, char* argv[])
   static const std::string PLANNING_GROUP = "interbotix_arm";
   
   // The
-  // :moveit_codedir:`MoveGroupInterface<moveit_ros/planning_interface/move_group/include/moveit/move_group/move_group.h>`
+  // :moveit_codedir:`MoveGroupInterface<moveit_ros/planning_interface/move_group_interface/include/moveit/move_group_interface/move_group_interface.h>`
   // class can be easily set up using just the name of the planning group you would like to control and plan for.
-  moveit::planning_interface::MoveGroupInterface move_group(move_group_node, PLANNING_GROUP);
+  moveit::planning_interface::MoveGroupInterface move_group_interface(node, PLANNING_GROUP);
 
   // We will use the
   // :moveit_codedir:`PlanningSceneInterface<moveit_ros/planning_interface/planning_scene_interface/include/moveit/planning_scene_interface/planning_scene_interface.h>`
   // class to add and remove collision objects in our "virtual world" scene
-  moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
+  //moveit::planning_interface::PlanningSceneInterface planning_scene_interface;
 
   // Raw pointers are frequently used to refer to the planning group for improved performance.
   //const moveit::core::JointModelGroup* joint_model_group =
-  //    move_group.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
+  //    move_group_interface.getCurrentState()->getJointModelGroup(PLANNING_GROUP);
 
-  // Set a target Pose
-  auto const target_pose = [] {
-    geometry_msgs::msg::Pose msg;
-    msg.position.x = 0.6;
-    msg.position.y = 0.0;
-    msg.position.z = 0.5;
-    return msg;
-  }();
-  move_group.setPoseTarget(target_pose);
+  node->setHomePoseTarget(move_group_interface);
+  
 
   // Create a plan to that target pose
-  auto const [success, plan] = [&move_group] {
+  auto const [success, plan] = [&move_group_interface] {
     moveit::planning_interface::MoveGroupInterface::Plan msg;
-    auto const ok = static_cast<bool>(move_group.plan(msg));
+    auto const ok = static_cast<bool>(move_group_interface.plan(msg));
     return std::make_pair(ok, msg);
   }();
 
   // Execute the plan
   if (success)
   {
-    move_group.execute(plan);
+    move_group_interface.execute(plan);
   }
   else
   {
     RCLCPP_ERROR(logger, "Planning failed!");
   }
+
+
+      
 
   // Shutdown ROS
   rclcpp::shutdown();
