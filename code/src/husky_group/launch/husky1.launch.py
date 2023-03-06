9963ebf51fa8923acdadb6d194f428d77bab8d0f
Orjan <orjano18@uia.no>
2023-03-06 15:25:23 +0100

    need imu

--- code/src/husky_group/launch/husky1.launch.py
+++ code/src/husky_group/launch/husky1.launch.py
@@ -184,12 +184,12 @@ def generate_launch_description():
     ld.add_action(node_controller_manager)
     ld.add_action(spawn_controller)
     ld.add_action(spawn_husky_velocity_controller)
-    #ld.add_action(launch_husky_control)
+    ld.add_action(launch_husky_control)
     ld.add_action(launch_husky_teleop_base)
     ld.add_action(launch_husky_teleop_joy)
 
     # Launch imu and lidar
-    #ld.add_action(node_um7_imu)
+    ld.add_action(node_um7_imu)
     ld.add_action(launch_ouster_lidar)
     
     return ld
