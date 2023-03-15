cbaa48d67de2754e4bec9140521acbf38ee5e53e
Orjano <orjano18@uia.no>
2023-03-15 14:44:53 +0100

    Switched to submodules

--- code/src/apriltag_ros/src/tag_functions.hpp
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <apriltag.h>
-#include <string>
-#include <unordered_map>
-
-
-extern const std::unordered_map<std::string, std::pair<apriltag_family_t* (*) (void), void (*)(apriltag_family_t*)>> tag_fun;
