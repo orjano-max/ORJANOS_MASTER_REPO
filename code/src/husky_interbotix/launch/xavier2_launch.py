1a1b1bb0d3a169a3d022357bab266c9e7bad6c05
Orjano <orjano18@uia.no>
2023-03-26 12:02:12 +0200

    [husky_interbotix] moved manipulator to xavier2

--- code/src/husky_interbotix/launch/xavier2_launch.py
+++ code/src/husky_interbotix/launch/xavier2_launch.py
@@ -8,6 +8,10 @@ from launch_ros.actions import Node
 from launch_ros.substitutions import FindPackageShare
 
 def generate_launch_description():
+
+    interbotix_urdf_extras_path = PathJoinSubstitution(
+                [FindPackageShare("husky_interbotix"), "urdf", "interbotix_urdf_extras.urdf"]
+    )
           
     # Launch Realsense camera
     launch_realsense_camera = IncludeLaunchDescription(
@@ -21,8 +25,41 @@ def generate_launch_description():
             [FindPackageShare('husky_interbotix'), 'launch', 'continuous_detection.launch.xml'])),
     )
 
+    launch_interbotix_moveit = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(PathJoinSubstitution(
+            [FindPackageShare('interbotix_xsarm_moveit'), 'launch', 'xsarm_moveit.launch.py'])),
+            launch_arguments ={
+            'robot_model' : 'vx300',
+            'use_moveit_rviz' : 'false',
+            'external_urdf_loc' : interbotix_urdf_extras_path,
+            }.items()
+    )
+     
+
+    # Static transform that places the interbotics robot on the husky
+    node_tf_manipulator = Node(
+        package="tf2_ros",
+        executable="static_transform_publisher",
+        arguments = ["-0.44", "-0.104", "0", "-1.5708", "0", "0", "user_rail_link", "world"],
+    )
+
+    # Publish scene geometry
+    launch_scene_geometry_publisher = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(PathJoinSubstitution(
+            [FindPackageShare('scene_geometry_publisher'), 'launch', 'scene_geometry_publisher_launch.py'])),
+            launch_arguments ={
+            'robot_model' : 'vx300',
+            }.items()
+    )
+
 
     ld = LaunchDescription()
+
+    # Launch Interbotix arm with moveit
+    ld.add_action(launch_interbotix_moveit)
+    ld.add_action(node_tf_manipulator)
+    # Publish scene
+    ld.add_action(launch_scene_geometry_publisher)
 
     # Launch Realsense camera
     ld.add_action(launch_realsense_camera)
